a5109aff35e114b229b2aa313d83ccb6e72e1be2
herman088 <hermancho08@gmail.com>
2021-07-04 19:01:54 +0800

    Add Login Function

--- app/src/main/java/sg/edu/np/madassignment/LoginActivity.java
+++ app/src/main/java/sg/edu/np/madassignment/LoginActivity.java
@@ -6,9 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button loginButton;
+    DBHandler dbHandler = new DBHandler(this, null, null, 1);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +31,47 @@ public class LoginActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+        loginButton = findViewById(R.id.button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText etUsername = findViewById(R.id.UserName);
+                EditText etPassword = findViewById(R.id.UserPassword);
+                //compare username and password
+                if(isValidCredentials(etUsername.getText().toString(), etPassword.getText().toString()))
+                {
+                    Intent intent = new Intent(LoginActivity.this, MainPage.class);
+                    startActivity(intent);
+                    Toast.makeText(LoginActivity.this, "Valid Credentials", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(LoginActivity.this, "Invalid Credentials!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
+
+
+    public boolean isValidCredentials(String username, String password)
+    {
+
+
+        User dbUsername = dbHandler.findUser(username);
+        if(dbUsername == null)
+        {
+            Toast.makeText(this, "User does not exist. Please Signup.", Toast.LENGTH_SHORT).show();
+
+        }
+        else
+        {
+            if(dbUsername.getUsername().equals(username) && dbUsername.getPassword().equals(password)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
