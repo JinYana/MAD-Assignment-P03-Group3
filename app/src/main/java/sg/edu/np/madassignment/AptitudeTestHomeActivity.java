50bbbcd98c7830e8c3aba0212e1bd42c713260db
JinYana <ngjinyang@gmail.com>
2021-07-12 18:45:44 +0800

    Finished mapping navbar to category page and aptitudetest page

--- app/src/main/java/sg/edu/np/madassignment/AptitudeTestHomeActivity.java
+++ app/src/main/java/sg/edu/np/madassignment/AptitudeTestHomeActivity.java
@@ -1,14 +1,18 @@
 package sg.edu.np.madassignment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class AptitudeTestHomeActivity extends AppCompatActivity {
 
@@ -16,6 +20,31 @@ public class AptitudeTestHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aptitude_test_home);
+
+
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        navigation.getMenu().getItem(1).setChecked(true);
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+
+                    case R.id.page_1:
+                        Intent a = new Intent(AptitudeTestHomeActivity.this,CategoryActivity.class);
+                        startActivity(a);
+
+
+                        break;
+
+                    case R.id.page_2:
+                        break;
+
+                }
+                return false;
+            }
+        });
 
 
         Button GoTest = findViewById(R.id.takeAptQuiz);
