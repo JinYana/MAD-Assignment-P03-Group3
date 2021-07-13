4d885f9405ec84f7d58edc4d9a2c199e3955ba6e
Ahmad Syufi Mikail <s10208124@connect.np.edu.sg>
2021-07-14 04:52:36 +0800

    Added in splash screen, have yet to add quote of the day feature.

---
+++ app/src/main/java/sg/edu/np/madassignment/SplashScreenActivity.java
@@ -0,0 +1,26 @@
+package sg.edu.np.madassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        int secondsDelayed = 1;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
+                finish();
+            }
+        }, secondsDelayed * 1000);
+    }
+}
